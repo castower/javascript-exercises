4c736a00aac54b0bd7527866728a85d7731669ac
Courtney <54308186+castower@users.noreply.github.com>
2023-06-22 23:44:34 -0400

    Complete 04_removeFromArray

--- 04_removeFromArray/README.md
+++ 04_removeFromArray/README.md
@@ -4,6 +4,8 @@ Implement a function that takes an array and some other arguments then removes t
 
 ```javascript
 removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
+
+//Check: npm test removeFromArray.spec.js
 ```
 
 ## Hints
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, drop1, drop2, drop3, drop4) {
+
+    dropList = [drop1, drop2, drop3, drop4];
+    dropList = dropList.filter(drop => drop != undefined);
+    array = array.filter(arrayItem => !dropList.includes(arrayItem)); 
+
+    return array
 
 };
+
+//console.log(removeFromArray([9, 8, 3, 0, 2, 8], 8));
 
 // Do not edit below this line
 module.exports = removeFromArray;
