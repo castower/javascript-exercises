ab7ede3d57d9888b028e594ace48c6fcd585e9a9
Courtney <54308186+castower@users.noreply.github.com>
2023-06-23 00:08:01 -0400

    Complete 05_sumAll

--- 05_sumAll/README.md
+++ 05_sumAll/README.md
@@ -4,6 +4,8 @@ Implement a function that takes 2 integers and returns the sum of every number b
 
 ```javascript
 sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
+
+//Check: npm test sumAll.spec.js
 ```
 
 
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+    numbers = [];
+    numbers.push(start);
+    numbers.push(end);
+    min = Math.min(...numbers);
+    max = Math.max(...numbers);
+
+    if (!(Number.isInteger(start) && Number.isInteger(end))) {
+        return "ERROR"
+    } else if (min >= 0 && max >= 0) {
+        sum = 0;
+        for (i=min; i < max +1; i++) {
+            sum += i;
+        }
+        return sum
+    } else {
+        return "ERROR"
+    }
+
 
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
