1961af37e43c2260504c1239fc87de32508cd5e3
Courtney <54308186+castower@users.noreply.github.com>
2023-06-23 01:06:11 -0400

    Completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,31 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  conversion = (temp - 32) * (5/9);
+  if (Number.isInteger(conversion)) {
+    conversionNum = conversion;
+  } else {
+    conversionNum = conversion.toFixed(1); 
+    conversionNum = parseFloat(conversionNum);
+  }
+
+  return conversionNum
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  conversion = (temp * (9/5)) + 32;
+  if (Number.isInteger(conversion)) {
+    conversionNum = conversion;
+  } else {
+    conversionNum = conversion.toFixed(1); 
+    conversionNum = parseFloat(conversionNum);
+  }
+  return conversionNum
 };
+
+console.log(convertToCelsius(32)) // fahrenheit to celsius, should return 0
+
+console.log(convertToFahrenheit(0)) // celsius to fahrenheit, should return 32
 
 // Do not edit below this line
 module.exports = {
