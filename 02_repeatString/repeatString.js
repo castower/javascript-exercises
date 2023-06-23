13e910a0f7826d45e2511e468b000ba44994c740
Courtney <54308186+castower@users.noreply.github.com>
2023-06-22 21:53:52 -0400

    Complete assignment 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+ 
+    output = "";
+    if (num >= 0) {
+        for (i=0; i < num; i++) {
+            output += string;
+        }
 
+        return output
+    } else {
+        return "ERROR"
+    }
 };
+
+//console.log(repeatString("hey", 3))
 
 // Do not edit below this line
 module.exports = repeatString;
