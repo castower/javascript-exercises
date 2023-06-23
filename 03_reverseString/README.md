91244652f552e311257c56facde64cff8972c8ab
Courtney <54308186+castower@users.noreply.github.com>
2023-06-22 22:30:23 -0400

    Complete assignment 03_reverseString

--- 03_reverseString/README.md
+++ 03_reverseString/README.md
@@ -4,7 +4,10 @@ Pretty simple, write a function called `reverseString` that returns its input, r
 
 ```javascript
 reverseString('hello there') // returns 'ereht olleh'
+
+//Check: npm test reverseString.spec.js
 ```
 
 ## Hints
 Strings in JavaScript cannot be reversed directly so you're going to have to split it into something else first.. do the reversal and then join it back together into a string.
+
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,25 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    reverse = [];
+    lastCharIndex = string.length;
+    stopIndex = lastCharIndex + 1;
+    for (i=0; i < string.length+1; i++) {
+        letterExtract = "";
+        letterExtract = string.substring(lastCharIndex, stopIndex);
+        //console.log(letterExtract);
+        reverse.push(letterExtract);
+        lastCharIndex -= 1;
+        stopIndex -= 1;
+        //console.log(lastCharIndex, stopIndex);
+    }
+
+    reversed = reverse.join("");
+
+    return reversed
 
 };
+
+//console.log(reverseString('hello there'));
 
 // Do not edit below this line
 module.exports = reverseString;
